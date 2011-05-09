6ddb31763d857342653b6d0bfdc64eabc79d0b9f
Endy Muhardin <endy.muhardin@gmail.com>
2011-05-09 14:31:25 +0700

    service interface
    Change-Id: Idebaeca772ae8839a2d8d16b52a5a41baf310be5

---
+++ src/main/java/bukualamat/service/BukuAlamatService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bukualamat.service;
+
+import bukualamat.entity.Grup;
+import bukualamat.entity.Kontak;
+import java.util.List;
+
+/**
+ *
+ * @author endy
+ */
+public interface BukuAlamatService {
+    public void simpan(Grup grup);
+    public void hapus(Grup grup);
+    public Grup cariGrupById(Long id);
+    public List<Grup> semuaGrup();
+    
+    
+    public void simpan(Kontak kontak);
+    public void hapus(Kontak kontak);
+    public Kontak cariKontakById(Long id);
+    public List<Kontak> semuaKontak();
+    public List<Kontak> cariKontak(Grup grup);
+}
