9a3fefc4cad4514d572f5febdc73ce7224593493
Endy Muhardin <endy.muhardin@gmail.com>
2011-05-11 15:30:06 +0700

    integrate spring context ke aplikasi web
    Change-Id: I75d88cc2bf0dc210c9f8b6996c9ba8067fd05435

--- src/main/java/bukualamat/ui/servlet/DaftarGrup.java
+++ src/main/java/bukualamat/ui/servlet/DaftarGrup.java
@@ -1,17 +1,32 @@
 package bukualamat.ui.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import bukualamat.entity.Grup;
+import bukualamat.service.BukuAlamatService;
+
 @SuppressWarnings("serial")
 public class DaftarGrup extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
+		ApplicationContext ctx = WebApplicationContextUtils
+		.getWebApplicationContext(req.getSession().getServletContext());
+		
+		BukuAlamatService service = 
+			(BukuAlamatService) ctx.getBean("bukuAlamatService");
+		
+		List<Grup> semuaGrup = service.semuaGrup();
 		
 		resp.setContentType("text/html");
 		
@@ -26,23 +41,18 @@ public class DaftarGrup extends HttpServlet {
 		output += "<tr>";
 		output += "<td>Kode</td>";
 		output += "<td>Nama</td>";
+		output += "<td>&nbsp;</td>";
 		output += "</tr>";
 
-		output += "<tr>";
-		output += "<td>G-001</td>";
-		output += "<td>Grup 001</td>";
-		output += "</tr>";
-
-		output += "<tr>";
-		output += "<td>G-001</td>";
-		output += "<td>Grup 001</td>";
-		output += "</tr>";
-
-		output += "<tr>";
-		output += "<td>G-001</td>";
-		output += "<td>Grup 001</td>";
-		output += "</tr>";
-
+		for (Grup grup : semuaGrup) {
+			output += "<tr>";
+			output += "<td>"+grup.getKode()+"</td>";
+			output += "<td>"+grup.getNama()+"</td>";
+			output += "<td><a href='edit?id="+grup.getId()+"'>edit</a>";
+			output += " | <a href='delete?id="+grup.getId()+"'>delete</a></td>";
+			output += "</tr>";
+		}
+		
 		output += "</table>";
 		
 		output += "</body>";
